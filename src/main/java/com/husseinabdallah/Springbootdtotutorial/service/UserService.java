9d0d8878a7f2442d4a8c565cc29bcf5dcb01b770
mishobo <60912349+mishobo@users.noreply.github.com>
2023-01-04 11:42:25 +0300

    userService get UserLocationDTO

--- src/main/java/com/husseinabdallah/Springbootdtotutorial/service/UserService.java
+++ src/main/java/com/husseinabdallah/Springbootdtotutorial/service/UserService.java
@@ -2,14 +2,25 @@ package com.husseinabdallah.Springbootdtotutorial.service;
 
 import com.husseinabdallah.Springbootdtotutorial.dto.UserLocationDTO;
 import com.husseinabdallah.Springbootdtotutorial.model.User;
+import com.husseinabdallah.Springbootdtotutorial.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-public class UserRepository {
+public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<UserLocationDTO> getAllUsersLocation(){
+        return userRepository.findAll()
+                .stream()
+                .map(this::convertingEntityToDTO)
+                .collect(Collectors.toList());
+    }
 
     public UserLocationDTO convertingEntityToDTO(User user){
         UserLocationDTO userLocationDTO = new UserLocationDTO();
